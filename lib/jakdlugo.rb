3c53f79ed87c465f9a4f3b320589924892683f54
coalest <c.streiff@pm.me>
2022-01-26 16:27:47 +0100

    fixed require paths

--- lib/jakdlugo.rb
+++ lib/jakdlugo.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 require_relative "jakdlugo/version"
+require_relative "jakdlugo/cli"
 
 module Jakdlugo
   class Error < StandardError; end
-  # Your code goes here...
 end
