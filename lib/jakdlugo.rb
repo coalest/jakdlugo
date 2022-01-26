0286c9cffd9fdb288599f16122772a73e8a88a05
coalest <c.streiff@pm.me>
2022-01-26 15:38:20 +0100

    $ bundle gem jakdlugo

---
+++ lib/jakdlugo.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "jakdlugo/version"
+
+module Jakdlugo
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/jakdlugo/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Jakdlugo
+  VERSION = "0.1.0"
+end
